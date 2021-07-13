3087535c24dfde799b647207e710a4132c6e122d
Doneowth <don.liang2021@gmail.com>
2021-07-13 16:43:25 -0500

    init customized test code for meowth code

--- FacialRecognition/02_face_training.py
+++ FacialRecognition/02_face_training.py
@@ -1,12 +1,12 @@
 ''''
 Training Multiple Faces stored on a DataBase:
-	==> Each face should have a unique numeric integer ID as 1, 2, 3, etc                       
+	==> Each face should have a unique numeric integer ID as 1, 2, 3, etc
 	==> LBPH computed model will be saved on trainer/ directory. (if it does not exist, pls create one)
 	==> for using PIL, install pillow library with "pip install pillow"
 
-Based on original code by Anirban Kar: https://github.com/thecodacus/Face-Recognition    
+Based on original code by Anirban Kar: https://github.com/thecodacus/Face-Recognition
 
-Developed by Marcelo Rovai - MJRoBot.org @ 21Feb18   
+Developed by Marcelo Rovai - MJRoBot.org @ 21Feb18
 
 '''
 
@@ -16,38 +16,49 @@ from PIL import Image
 import os
 
 # Path for face image database
-path = 'dataset'
+# path = 'dataset'
+path = 'machi'
 
 recognizer = cv2.face.LBPHFaceRecognizer_create()
-detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml");
+# detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+detector = cv2.CascadeClassifier("haarcascade_frontalcatface_default.xml")
 
 # function to get the images and label data
 def getImagesAndLabels(path):
+    file_names = []
+    for f in os.listdir(path):
+        if f == ".DS_Store":
+            continue
+        file_names.append(f)
+    print(file_names)
 
-    imagePaths = [os.path.join(path,f) for f in os.listdir(path)]     
+    imagePaths = [os.path.join(path, f) for f in file_names]
     faceSamples=[]
     ids = []
 
     for imagePath in imagePaths:
 
         PIL_img = Image.open(imagePath).convert('L') # convert it to grayscale
-        img_numpy = np.array(PIL_img,'uint8')
+        img_numpy = np.array(PIL_img, 'uint8')
 
         id = int(os.path.split(imagePath)[-1].split(".")[1])
         faces = detector.detectMultiScale(img_numpy)
 
-        for (x,y,w,h) in faces:
-            faceSamples.append(img_numpy[y:y+h,x:x+w])
+        for (x, y, w, h) in faces:
+            faceSamples.append(img_numpy[y:y+h, x:x+w])
             ids.append(id)
 
-    return faceSamples,ids
+    return faceSamples, ids
 
 print ("\n [INFO] Training faces. It will take a few seconds. Wait ...")
-faces,ids = getImagesAndLabels(path)
+faces, ids = getImagesAndLabels(path)
 recognizer.train(faces, np.array(ids))
 
 # Save the model into trainer/trainer.yml
-recognizer.write('trainer/trainer.yml') # recognizer.save() worked on Mac, but not on Pi
+# recognizer.write('trainer/trainer_human.yml')  # recognizer.save() worked on Mac, but not on Pi
+recognizer.write('trainer/trainer_cat.yml')  # recognizer.save() worked on Mac, but not on Pi
 
 # Print the numer of faces trained and end program
 print("\n [INFO] {0} faces trained. Exiting Program".format(len(np.unique(ids))))
+
+
--- FacialRecognition/03_face_recognition.py
+++ FacialRecognition/03_face_recognition.py
@@ -9,65 +9,110 @@ Developed by Marcelo Rovai - MJRoBot.org @ 21Feb18
 '''
 
 import cv2
+import time
 import numpy as np
-import os 
+import os
 
 recognizer = cv2.face.LBPHFaceRecognizer_create()
-recognizer.read('trainer/trainer.yml')
+recognizer.read('trainer/trainer_human.yml')
+recognizer.read('trainer/trainer_cat.yml')
+
 cascadePath = "haarcascade_frontalface_default.xml"
-faceCascade = cv2.CascadeClassifier(cascadePath);
+faceCascade = cv2.CascadeClassifier(cascadePath)
+
+cat_cascadePath = "haarcascade_frontalcatface_default.xml"
+cat_faceCascade = cv2.CascadeClassifier(cat_cascadePath)
 
 font = cv2.FONT_HERSHEY_SIMPLEX
 
-#iniciate id counter
+# iniciate id counter
 id = 0
 
 # names related to ids: example ==> Marcelo: id=1,  etc
-names = ['None', 'Marcelo', 'Paula', 'Ilza', 'Z', 'W'] 
+names = ['None', 'Don', 'X', 'Machi', 'Z', 'W']
 
 # Initialize and start realtime video capture
 cam = cv2.VideoCapture(0)
-cam.set(3, 640) # set video widht
-cam.set(4, 480) # set video height
+cam.set(3, 640)  # set video widht
+cam.set(4, 480)  # set video height
 
 # Define min window size to be recognized as a face
-minW = 0.1*cam.get(3)
-minH = 0.1*cam.get(4)
+minW = 0.1 * cam.get(3)
+minH = 0.1 * cam.get(4)
+time.sleep(2)
 
 while True:
+    ret, frame = cam.read()
+    # img = cv2.flip(img, -1) # Flip vertically
+    if frame is None:
+        print("empty frame")
 
-    ret, img =cam.read()
-    img = cv2.flip(img, -1) # Flip vertically
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-
-    faces = faceCascade.detectMultiScale( 
+    faces = faceCascade.detectMultiScale(
         gray,
-        scaleFactor = 1.2,
-        minNeighbors = 5,
-        minSize = (int(minW), int(minH)),
-       )
+        scaleFactor=1.2,
+        minNeighbors=5,
+        minSize=(int(minW), int(minH)),
+    )
 
-    for(x,y,w,h) in faces:
+    cat_faces = cat_faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.2,
+        minNeighbors=5,
+        minSize=(int(minW), int(minH)),
+    )
 
-        cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
+    human_name = "unknown human"
+    for (x, y, w, h) in faces:
 
-        id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-        # Check if confidence is less them 100 ==> "0" is perfect match 
+        id, confidence = recognizer.predict(gray[y:y + h, x:x + w])
+
+        # Check if confidence is less them 100 ==> "0" is perfect match
         if (confidence < 100):
-            id = names[id]
             confidence = "  {0}%".format(round(100 - confidence))
-        else:
-            id = "unknown"
-            confidence = "  {0}%".format(round(100 - confidence))
-        
-        cv2.putText(img, str(id), (x+5,y-5), font, 1, (255,255,255), 2)
-        cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)  
-    
-    cv2.imshow('camera',img) 
+            # if id < len(names):
+            #     human_name = names[id % 5]
+            #     confidence = "  {0}%".format(round(100 - confidence))
+            # else:
+            #     print("index out of bounds human id: {}".format(id))
 
-    k = cv2.waitKey(10) & 0xff # Press 'ESC' for exiting video
+        else:
+            human_name = "? human"
+            confidence = "  {0}%".format(round(100 - confidence))
+
+        cv2.putText(frame, str("Don"), (x + 5, y - 5), font, 1, (255, 255, 255), 2)
+        cv2.putText(frame, str(confidence), (x + 5, y + h - 5), font, 1, (255, 255, 0), 1)
+
+    cat_name = "unknown cat"
+    # for (x, y, w, h) in cat_faces:
+    #
+    #     cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    #
+    #     cid, confidence = recognizer.predict(gray[y:y + h, x:x + w])
+    #
+    #     # Check if confidence is less them 100 ==> "0" is perfect match
+    #     if (confidence < 100):
+    #         confidence = "  {0}%".format(round(100 - confidence))
+    #         # if cid < len(names):
+    #         #     cat_name = names[cid]
+    #         #     confidence = "  {0}%".format(round(100 - confidence))
+    #         # else:
+    #         #     print("index out of bounds cat id: {}".format(cid))
+    #
+    #     else:
+    #         cat_name = "? cat"
+    #         confidence = "  {0}%".format(round(100 - confidence))
+
+    #   cv2.putText(frame, str("DuoDuo"), (x + 5, y - 5), font, 1, (255, 255, 255), 2)
+    #   cv2.putText(frame, str(confidence), (x + 5, y + h - 5), font, 1, (255, 255, 0), 1)
+
+    cv2.imshow('camera', frame)
+
+    # Press 'ESC' for exiting video
+    k = cv2.waitKey(10) & 0xff
     if k == 27:
         break
 
