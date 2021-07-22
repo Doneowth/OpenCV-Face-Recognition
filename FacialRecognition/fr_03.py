2be8c0370c7946534510338bc3f432514ee42b43
Doneowth <don.liang2021@gmail.com>
2021-07-22 15:47:10 -0500

    update comments

--- FacialRecognition/fr_03.py
+++ FacialRecognition/fr_03.py
@@ -27,6 +27,7 @@ print("\n [INFO] Initializing face capture. Look the camera and wait ...")
 # Initialize individual sampling face count
 count = 0
 
+# for both cat and don
 while True:
     ret, img = cam.read()
     # time.sleep(5)
